aaa8c4a2af60c9770629b53a5a156283c5c7f35b
Fraol <fraolkelbessa@gmail.com>
2020-03-17 23:20:48 +0300

    added storage support

---
+++ common/models/storage.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function(Storage) {
+
+    // Storage.remoteMethod('deleteFile',{
+    //     accepts:[
+    //         {arg: 'id', type: 'string', required: true}
+    //     ],
+    //     returns:{arg:'id',type:'string'},
+    //     http: {verb: 'get', path: '/deleteFile'}
+    // })
+
+    // Storage.deleteFile =function(id,cb){
+    //     Storage.delete()
+    //     cb(null,"null")
+    // }
+
+};
